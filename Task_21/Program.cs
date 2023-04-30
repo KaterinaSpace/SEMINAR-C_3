acb2fc0d25308bc82da26c496e6aad7404500991
Katerina <Katarinka98@inbox.ru>
2023-04-30 18:21:10 +0300

    add task21 mod 2

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -12,13 +12,13 @@
 /* sqrt((x2 - x1)^2 + (y2-y1)^2) */
 
 Console.WriteLine("Введите координаты  x1");
-int x1 = int.Parse(Console.ReadLine());
+int x1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаты y1");
-int y1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаты x2");
-int x2 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаты y2");
-int y2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine()!);
 
 double answer = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 //double sqr1 = Math.Pow((x1-x2), 2);    вариант 2 
